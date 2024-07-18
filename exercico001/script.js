094a5c1cbe62a8b71d002635b63c0afb14452246
jonatas <jonatas_dev@hotmail.com>
2024-07-18 20:00:10 -0300

    primeiro exercicio sozinho

---
+++ exercico001/script.js
@@ -0,0 +1,10 @@
+let nome = document.getElementById('txtn')
+let senha = document.getElementById('txts')
+let email = document.getElementById('txtemail')
+function acessar(){
+    let n = nome
+    let s = senha
+    let e = email
+    
+}
+
