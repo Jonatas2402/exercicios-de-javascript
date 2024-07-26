dbed28b2c3b9f01c28768abde47a322a0e8b9731
jonatas <jonatas_dev@hotmail.com>
2024-07-26 17:34:24 -0300

    final loops

--- ex.loops/repetições/node_e8907673b52d3.tmp.js
+++
@@ -1,9 +0,0 @@
-//while loops
-//será execitado determinado código enquanto uma condição for verdadeira.
-
-let index = 0
-
-while(index < 10) {
-    console.log('index é menor que 10');
-    //index = index + 1
-}
