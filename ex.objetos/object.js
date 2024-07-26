76bcbdca3d0280b729e4b82900184d7414a68d27
jonatas <jonatas_dev@hotmail.com>
2024-07-26 15:47:19 -0300

    inicio de JSON

--- ex.objetos/object.js
+++ ex.objetos/object.js
@@ -5,18 +5,62 @@ const pessoa = {
     sobreNome: 'silva',
     idade: 26,
     profissão: 'programador',
-    hobbies: ['ler', 'estudar', 'jogar']
-};
+    hobbies: ['ler', 'estudar', 'jogar'],
+    cat: {
+        name: 'alex',
+        idade: 2
 
-let primeiroNome = pessoa.primeiroNome
-let sobreNome = pessoa.sobreNome
-let idade = pessoa.idade
-let hobbies = pessoa.hobbies[1]
+        
+    }
+};
+/*const primeiroNome = pessoa.primeiroNome;
+const sobreNome = pessoa.sobreNome;
+const idade = pessoa.idade;
+const profissão = pessoa.profissão;
+const hobbies = pessoa.hobbies; */
+
+//mesma coisa que acima
+//também temos como renomear, como foi renomeado a idae para age
+const {idade: age, primeiroNome, sobreNome, hobbies, profissão, cat:{name: catname}} = pessoa
 
 console.log(primeiroNome);
 console.log(sobreNome);
-console.log(idade);
+console.log(age);
 console.log(hobbies);
+
+//adicionar mais um elemento ao objeto
+/*pessoa.cat = 'alex'*/
+//assim você pode colocar os dados do novo elemento direto no objeto
+console.log(pessoa.cat.idade);
+//podemos acessar qualquer propriedade do objeto.
+console.log(catname);
+
+
+
+
+const todos = [
+    {
+        id: 1,
+        descrição: 'estudar programação',
+        completa: false
+    },
+    {
+        id: 2,
+        descrição: 'estudar inglês',
+        completa: true,
+    },
+    {
+        id: 3,
+        descrição: 'treinar',
+        completa: true,
+    }
+]
+
+const res = todos [2].descrição;
+
+console.log(res);
+
+
 
 
 
