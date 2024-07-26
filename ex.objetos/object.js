040996a653736192b355fb70de99afbe3be2bf4d
jonatas <jonatas_dev@hotmail.com>
2024-07-26 14:14:19 -0300

    inicio trabalho com listas

---
+++ ex.objetos/object.js
@@ -0,0 +1,24 @@
+//objetos gurada valores de uma propriedade
+
+const pessoa = {
+    primeiroNome: 'jonatas',
+    sobreNome: 'silva',
+    idade: 26,
+    profissão: 'programador',
+    hobbies: ['ler', 'estudar', 'jogar']
+};
+
+let primeiroNome = pessoa.primeiroNome
+let sobreNome = pessoa.sobreNome
+let idade = pessoa.idade
+let hobbies = pessoa.hobbies[1]
+
+console.log(primeiroNome);
+console.log(sobreNome);
+console.log(idade);
+console.log(hobbies);
+
+
+
+
+
