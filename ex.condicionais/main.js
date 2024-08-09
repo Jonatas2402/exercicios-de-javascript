2f8b575dc659b2570413d0f297b6822c3ef389ca
jonatas <jonatas_dev@hotmail.com>
2024-08-09 13:20:22 -0300

    inicio condicionais

---
+++ ex.condicionais/main.js
@@ -0,0 +1,27 @@
+
+
+const sum = 1+1
+
+if (sum === 2) {
+    console.log('sum é igual a 2!');
+    
+}else {
+    console.log('sum is not two!');
+    
+}
+//uso de and e or
+//&& = and(e)
+//|| = or(ou)
+ 
+const sum1 = 2+2
+const sum2 = 3+10
+
+if(sum1 === 4 && sum2 === 6) {
+    console.log('sum is 4 and sum2 is 6!');   
+}
+
+if(sum1 === 4 || sum2 === 6) {
+    console.log('sum is 4 or sum2 is 6!'); 
+}
+
+
---
+++ ex.condicionais/op_ternarios.js
@@ -0,0 +1,13 @@
+const sum = 1 + 2
+
+//operadores ternarios
+
+let number = sum === 2 ? 2 : 4; 
+
+/*if(sum ===2){
+    number =2
+}else {
+    number = 4
+}*/
+
+console.log(number);
