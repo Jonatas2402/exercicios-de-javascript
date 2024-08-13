2baf531da568c98ea386373e5de64a6bd2946adf
jonatas <jonatas_dev@hotmail.com>
2024-08-13 17:31:32 -0300

    inicio poo

---
+++ ex.poo/poo.js
@@ -0,0 +1,13 @@
+//class: basicamente impressoras de objetos
+
+class person {
+    constructor(firstsname, lastname, age) {
+        this.firstsname = firstsname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+}
+
+const person = new person ("jonatas", "silva", 26)
+console.log(person);
+
