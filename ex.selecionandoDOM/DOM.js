5e664c904c42bd849cf51dd7bdbf12fb24f1ef72
jonatas <jonatas_dev@hotmail.com>
2024-08-23 10:48:05 -0300

    final selecionar

--- ex.selecionandoDOM/DOM.js
+++ ex.selecionandoDOM/DOM.js
@@ -1,5 +1,10 @@
-let add = document.getElementById('add-user');
+//let add = window.document.getElementById('add-user');
+const add = document.querySelector('#add-user')
+//add.textContent = "adicionar usuário"
+add.innerText = "adicionar usuário"
 
-console.log(add);
 
+// Selecionar múltiplos elementos
+
+const allItens = document.querySelectorAll('.item')
 
