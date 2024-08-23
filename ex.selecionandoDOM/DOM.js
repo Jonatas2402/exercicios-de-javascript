e5b1b1a9636604577600bb097590f41296d5b48c
jonatas <jonatas_dev@hotmail.com>
2024-08-23 10:50:26 -0300

    ajustes finais

--- ex.selecionandoDOM/DOM.js
+++ ex.selecionandoDOM/DOM.js
@@ -7,4 +7,7 @@ add.innerText = "adicionar usuário"
 // Selecionar múltiplos elementos
 
 const allItens = document.querySelectorAll('.item')
+// mais utilizado pois transforma os itens selecionados em lista, mais fácil a manipulação.
 
+
+//Manipulando itens com DOM.
