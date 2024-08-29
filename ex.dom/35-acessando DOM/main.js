8f08fbcb7cfb513986800051c28bee072a73065f
jonatas <jonatas_dev@hotmail.com>
2024-08-29 17:03:28 -0300

    dom

---
+++ ex.dom/35-acessando DOM/main.js
@@ -0,0 +1,20 @@
+//Acessando DOM
+
+// Tag
+let titulo = document.getElementsByTagName('h1')[0]
+
+console.log(titulo);
+
+
+// id
+
+let paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+// class
+
+let itens = document.getElementsByClassName('item');
+
+console.log(itens);
+
---
+++ ex.dom/36-querySelector/main.js
@@ -0,0 +1,13 @@
+let itensClass = document.getElementsByClassName('item');
+
+console.log(itensClass);
+
+// Utilizando querySelectorAll
+
+let queryItens = document.querySelectorAll('#lista2 li');
+
+console.log(queryItens);
+
+let queryItens2 = document.querySelectorAll('#lista .item');
+
+console.log(queryItens2);
